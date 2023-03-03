4179de190839f77a902fd8e46c789baf19755b75
ktsivkov <ktsivkov@gmail.com>
2023-03-03 16:43:03 +0100

    Improved test_verifier.sh with fixed bugs, and better .coverignore file handling

--- Readme.md
+++ Readme.md
@@ -13,3 +13,35 @@ This is an example project that targets to show how you can implement a CI/CD pi
 - `make test` - runs all tests and checks the test coverage
 - `make build` - builds and prepares a docker image for the application
 - `make run` - runs the application using the built docker image
+
+## The `.coverignore` file
+As at the moment GoLang doesn't provide any official way to enforce the test existence I have created
+a bash script that enforces each `*.go` file to have its own `*_test.go` one.
+Check `./cicd/test_verifier.sh`
+
+On the other hand, as most applications have a few files that are not supposed to be counted
+in the test coverage.
+
+Therefore, I have implemented a way to ignore such files using the `./.coverignore` file,
+located in the project root.
+
+When you run `./cicd/test_verifier.sh` from the project root, it reads the contents
+of the `./.coverignore` file, and ignores them.
+
+The syntax used is similar to the `.gitignore`'s one.
+
+Example `.coverignore` file and syntax / note that comments are not supposed to be in the file,
+but I am using them to make it more understandable for the sake of the example.
+```
+.                     # will ignore everything
+.*                    # will ignore everything
+./                    # will ignore everything
+./*                   # will ignore everything
+./*.go                # will ignore all .go files located in the main directory
+./main.go             # will ignore the ./main.go file from the test coverage
+./mydir/demofile.go   # will ignore the demofile.go located inside mydir
+./mydir/*.go          # will ignore all .go files located inside mydir, but not its subdirectories
+./mydir/*             # will ignore all files located inside mydir, and its subdirectories
+./mydir/*/            # will ignore all files located in its subdirectories
+...
+```
--- cicd/test_verifier.sh
+++ cicd/test_verifier.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
-# Declare an exit code variable, in order to fail in case any untested files were found
-exit_code=0
 
-# Read the excluded files into an array
-mapfile -t excluded_files < .coverignore
+ignore_file=".coverignore"
+untested_files=()
 
-# Function to check if a file or directory is excluded
-function is_excluded() {
-  local file=$1
-  for excluded_file in "${excluded_files[@]}"; do
-    if [[ -n "$excluded_file"       && (
-      "$file" == "$excluded_file"   ||
-      "$file/" == "$excluded_file"  ||
-      "$file" == $excluded_file     ||
-      ( $excluded_file == *"/"      && $file == "$excluded_file"* )
-    ) ]]; then
-      return 0
-    fi
-  done
-  return 1
+# Read the .coverignore file for this directory into an array
+ignore_list=()
+if [[ -f "$ignorefile" ]]; then
+  mapfile -t ignore_list < "$ignorefile"
+fi
+
+count_slashes() {
+  return $(echo "$1" | tr -cd '/' | wc -c)
 }
 
-# Loop over all directories and files
-while IFS= read -r -d '' file; do
-  # Check if the file was excluded, and if it is a .go file without a matching _test.go file
-  if ! is_excluded "$file" && [[ "$file" == *.go && ! "$file" == *_test.go && ! -f "${file%.*}_test.go" ]]; then
-    exit_code=1
-    echo "ERROR! File $file does not have a matching _test.go file"
-  fi
-done < <(find . -type f -print0)
+process_directory() {
+  local dir="$1"
 
-exit $exit_code
+  # Loop through each file or directory in the directory
+  for entry in "$dir"/*; do
+    # Check if the file or directory matches any of the ignoring patterns
+    for pattern in ${ignore_list[@]}; do
+      if [[ ${pattern: -1} == "/" ]]; then
+        pattern="${pattern%/*}"
+      fi
+      # Check if the entry matches the file pattern
+      if [[ ( "$entry" == "$pattern" && $(count_slashes "$entry") -eq $(count_slashes "$pattern") ) ||
+            "$dir" == "$pattern"  ]]; then
+        continue 2 # An ignoring pattern was matched, so we ignore
+      fi
+    done
+
+    # If the entry is a directory, recursively process it
+    if [[ -d $entry ]]; then
+      process_directory "$entry"
+      continue
+    fi
+
+    if [[ $entry =~ \.go$ && ! $entry =~ _test\.go$ && ! -f ${entry%.*}_test.go ]]; then
+      untested_files+=("$entry")
+    fi
+  done
+}
+
+# Start processing the root directory
+process_directory "."
+
+if [[ ${#untested_files[@]} -eq 0 ]]; then
+  exit 0
+fi
+
+for file in "${untested_files[@]}"; do
+  echo "No tests found for $file"
+done
+exit 1
